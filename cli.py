50f849a1ea816605e07169f86eccbe4b5c15f1ba
chewarer <chewarer@yandex.ru>
2020-03-04 20:50:55 +0100

    Command line interface for tasks.

---
+++ cli.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import click
+import asyncio
+from functools import wraps
+
+
+def coro(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return asyncio.run(f(*args, **kwargs))
+    return wrapper
+
+
+@click.group(name='stocks')
+def group_stocks():
+    """Stocks commands"""
+
+
+@click.group(name='idea')
+def group_ideas():
+    """Ideas commands"""
+
+
+@click.group()
+def entire_groups():
+    """Command line interface"""
+
+
+entire_groups.add_command(group_stocks)
+entire_groups.add_command(group_ideas)
+
+
+@group_stocks.command()
+@coro
+@click.option('-d', '--date', default='2020-12-31', type=str, help='Date for parse')
+async def get_stocks(date):
+    """Get stock quotes from Moscow Exchange ISS server"""
+    from backend.tasks.iss_stock_quotes import get_for_date
+    await get_for_date(date)
+
+
+@group_ideas.command()
+@coro
+@click.option('-l', '--limit', default=20, type=int, help='Limit for parse')
+async def tinkoff(limit):
+    """Get ideas from Tinkoff"""
+    from backend.scrappers.tinkoff import parse_ideas
+    await parse_ideas(limit)
+
+
+@group_ideas.command()
+@coro
+@click.option('-l', '--limit', default=20, type=int, help='Limit for parse')
+@click.option('-o', '--offset', default=0, type=int, help='Offset for parse')
+async def bks(limit, offset):
+    """Get ideas from BKS"""
+    from backend.scrappers.bks import parse_ideas
+    await parse_ideas(limit, offset)
+
+
+if __name__ == '__main__':
+    entire_groups()
